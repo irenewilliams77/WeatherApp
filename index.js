6910ac038e8e72e6c2a025e6e1306a88b2dfd3b9
Irene Williams <europeandistribution@ymail.com>
2020-09-02 13:31:03 -0400

    created weather app

---
+++ index.js
@@ -0,0 +1,23 @@
+var button = document.querySelector('.submit');
+var inputValue = document.querySelector('.inputvalue');
+var name = document.querySelector('.name');
+var desc = document.querySelector('.desc');
+var temp = document.querySelector('.temp');
+
+button.addEventListener('click', function(){
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+ inputValue.value +'&appid=20ab93081b8029772be804bb0da7de53')
+
+    .then(response => response.json())
+    .then(data => {
+        var nameValue = data['name'];
+        var tempValue = data['main']['temp'];
+        var descValue = data['weather']['main'];
+    
+        name.innerHTML = nameValue;
+        temp.innerHTML = tempValue;
+        desc.innerHTML = descValue;
+    })
+    
+    .catch (err => alert("City does not exist"))
+})
+
